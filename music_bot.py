1c644051988d3c78fa8f9e78555f9b65b09e2d14
Ben Standerline <46759388+thelioncape@users.noreply.github.com>
2021-05-16 14:57:40 +0100

    Fix setPlaybackSpeedMessage

--- music_bot.py
+++ music_bot.py
@@ -33,6 +33,7 @@ serverDied = False
 def setPlaybackSpeedMesage(playbackSpeed):
     global playbackSpeedMessage
     playbackSpeedMessage = "<p>Playback speed: {0}x</p>".format(str(playbackSpeed))
+    updateComment()
 
 def cleanhtml(raw_html):
   cleanr = re.compile('<.*?>')
