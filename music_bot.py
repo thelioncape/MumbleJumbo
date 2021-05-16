52d265067f52c73d10b721775663c6475c4f495c
Ben Standerline <46759388+thelioncape@users.noreply.github.com>
2021-05-16 17:27:16 +0100

    Refactor into classes

--- music_bot.py
+++ music_bot.py
@@ -1,177 +1,289 @@
 #!/usr/bin/python3
 
-import pymumble_py3
+import pymumble_py3 as pymumble
+from pymumble_py3 import callbacks as cbks
 import subprocess as sp
 import audioop, time
 import json
 import re
 
-with open("config.json") as f:
-	config = json.load(f)
+"""
+Main bot class
+Contains actions required to load the bot and control actions
+"""
+class MumbleBot:
+	def __init__(self): # Defines configuration file
+		with open("config.json") as f:
+			config = json.load(f)
+		
+		self.messages = Messages()
+		self.configurables = Configurables(config, self.messages)
+		self.callbacks = Callbacks(self)
 
-server = config["host"]
-nick = config["user"]
-passwd = config["password"]
-port = config["port"]
-certfile = config["certfile"]
-keyfile = config["keyfile"]
+	def startBot(self):
+		self.mumble = pymumble.Mumble(
+			host	=	self.configurables.SERVER,
+			user	=	self.configurables.NICK,
+			port	=	self.configurables.PORT,
+			password=	self.configurables.PASSWD,
+			certfile=	self.configurables.CERTFILE,
+			keyfile	=	self.configurables.KEYFILE)
+		
+		# Set MESSAGE_RECEIVED callback
+		self.mumble.callbacks.set_callback(
+			cbks.PYMUMBLE_CLBK_TEXTMESSAGERECEIVED,
+			self.callbacks.message_received)
+		
+		# Set DISCONNECT callback
+		self.mumble.callbacks.set_callback(
+			cbks.PYMUMBLE_CLBK_DISCONNECTED,
+			self.callbacks.died)
 
-playbackspeed = 1 #Ensure input is sanitised if users are allowed to change this.
+		# Start bot engine
+		self.mumble.start()
 
-playing = False
-skip = False
-with open("helpmessage.html") as f:
-	helpmessage = f.read()
-	print(helpmessage)
-	print(type(helpmessage))
-queueMessage = "<p>Current Queue</p>\
-	\n<p>No songs in queue</p>"
-currentSongMessage = "<p>Currently Playing:</p>\
-	\n<p>Nothing!</p>"
-playbackSpeedMessage = "<p>Playback speed: 1x</p>"
-queue = []
-serverDied = False
+		# Wait for connection to complete to prevent thread clashes
+		self.mumble.is_ready()
 
-def setPlaybackSpeedMesage(playbackSpeed):
-	global playbackSpeedMessage
-	playbackSpeedMessage = "<p>Playback speed: {0}x</p>".format(str(playbackSpeed))
-	updateComment()
+		# Get bot's user as an object
+		self.botUserObj = self.mumble.users.myself
 
-def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
+		# Initial comment update
+		self.updateComment()
 
-def updateQueue():
-	global queueMessage
-	titles = []
-	for song in queue:
-		command = ["youtube-dl", "-e", song] # Get title
-		with sp.Popen(command, stdout=sp.PIPE) as p:
-			title = str(p.stdout.readline().decode('UTF-8').strip())
-		titles.append(title)
+	def updateComment(self): #TODO
+		self.configurables.updateQueue()
 
-	queueMessage = "<p>Current queue:</p>"
-	if len(titles) > 0:
-		for title in titles:
-			queueMessage += "\n<p>" + title + "</p>"
+		comment =  self.messages.currentSongMessage
+		comment += "<br />"
 
-def message_received(text):
-	global url, skip, queueMessage, queue
-	message = cleanhtml(text.message)
-	#user = mumble.users[text.actor]
+		comment += self.messages.queueMessage
+		comment += "<br />"
 
-	if message.startswith("!"):
-		if message.startswith("!play "):
-			queue.append(message[6::])
+		comment += self.messages.playbackSpeedMessage
+		comment += "<br />"
 
-			updateComment()
-		elif message == "!skip":
-			mumble.sound_output.clear_buffer()
-			skip = True
-			time.sleep(3)
-			updateComment()
-		elif message == "!yeet":
-			mumble.stop()
-			exit()
-		elif message == "!clear":
-			queue = []
-			time.sleep(1)
-			updateComment()
-		elif message == "!stopall":
-			queue = []
-			mumble.sound_output.clear_buffer()
-			skip = True
-			time.sleep(1)
-			updateComment()
-		elif message.startswith("!speed"):
-			try:
-				if speedchange(message[7:]):
-					mumble.users[text.actor].send_text_message("Speed changed to " + str(message[7:]) + "x")
-				else:
-					mumble.users[text.actor].send_text_message("Please provide a speed between 0.5 and 2")
-			except:
-				mumble.users[text.actor].send_text_message("Please provide a speed between 0.5 and 2")
+		comment += self.messages.helpmessage
 
+		self.botUserObj.comment(comment)
 
-def updateComment():
-	updateQueue()
+	def mainLoop(self):
+		while True:
+			print("Start Tick Processing")
+			print("Current queue:", self.configurables.queue)
+			if len(self.configurables.queue) > 0: # If there are songs queued
+				url = self.configurables.queue.pop(0) # Pop next item in queue
+				# Get title of next item in queue
+				command = ["youtube-dl", "-e", url]
+				with sp.Popen(command, stdout=sp.PIPE) as p:
+					self.messages.currentSongMessage = (
+						"<p>Currently Playing:</p>")
+					self.messages.currentSongMessage += "<p>"
+					self.messages.currentSongMessage += str(
+						p.stdout.readline().decode('UTF-8').strip())
+					self.messages.currentSongMessage += "</p>"
+				
+				# Begin downloading video to stdout pipe to wave_file
+				command = 	["youtube-dl",
+							"-f", "bestaudio",
+							url,
+							"--buffer-size", "2M",
+							"-o", "-"]
+				wave_file = sp.Popen(command, stdout=sp.PIPE).stdout
+				# Convert and play wave_file
+				command = 	["ffmpeg",
+							"-i", "-",
+							"-acodec", "pcm_s16le",
+							"-f", "s16le",
+							"-ab", "192k",
+							"-ac", "1",
+							"-ar", "48000",
+							"-fflags", "nobuffer",
+							"-filter:a",
+							"atempo=" + str(self.configurables.playbackSpeed),
+							"-"]
+				sound = sp.Popen(command,
+								stdout=sp.PIPE,
+								stderr=sp.DEVNULL,
+								stdin=wave_file, bufsize=1024)
+				
+				print("Playing")
+				self.configurables.playing = True
+				self.updateComment()
+				# Send audio bytes to Mumble sound queue and play
+				while True:
+					if self.configurables.skip: # Stop filling buffer if skip activated
+						self.configurables.skip = False
+						break
+					raw_music = sound.stdout.read(1024)
+					if not raw_music: # No more data -> buffer filled
+						break
+					# Add raw sound to mumble output buffer
+					self.mumble.sound_output.add_sound(raw_music)
+				print("Buffer filled")
+				self.updateComment()
 
-	comment = currentSongMessage
-	comment += "<br />"
-	comment += queueMessage
-	comment += "<br />"
-	comment += playbackSpeedMessage
-	comment += "<br />"
-	comment += helpmessage
+				# Wait for Mumble to output sound buffer to server
+				while self.mumble.sound_output.get_buffer_size() > 0.5:
+					time.sleep(0.01)
+				
+				# Song finished
+				self.configurables.playing = False
+				self.currentSongMessage = (
+					"<p>Currently Playing:</p>\n<p>Nothing!</p>")
+			print("Tick Processing Completed")
+			self.updateComment()
+			time.sleep(2)
+			if self.configurables.serverDied:
+				exit()
+				
 
-	botUser.comment(comment)
+			
 
-def died():
-	global serverDied
-	serverDied = True
+class Callbacks:
+	def __init__(self, mumblebot):
+		self.mumblebot = mumblebot
+		self.conf = self.mumblebot.configurables
+		self.messages = self.mumblebot.messages
 
-def speedchange(target):
-	try:
-		if float(target) <= 2 and float(target) >= 0.5:#Limits of the ffmpeg filter.
-			global playbackspeed
-			playbackspeed = target
-			setPlaybackSpeedMesage(float(target))
+	# Removes any HTML tags from a string using regex
+	# Used when cleaning incoming messages for processing
+	def cleanhtml(self, raw_html):
+		cleanr = re.compile("<.*?>")
+		cleantext = re.sub(cleanr, '', raw_html)
+		return cleantext
+
+	# Processes incoming messages to see if they are commands for us
+	def message_received(self, text):
+		message = self.cleanhtml(text.message)
+		actor = text.actor
+		#user = mumble.users[text.actor]
+
+		if message.startswith("!"):
+			if message.startswith("!play "):
+				self.conf.queue.append(message[6::])
+				self.mumblebot.updateComment()
+
+			elif message == "!skip":
+				self.mumblebot.mumble.sound_output.clear_buffer()
+				self.conf.skip = True
+				time.sleep(3)
+				self.mumblebot.updateComment()
+
+			elif message == "!yeet":
+				self.mumblebot.mumble.stop()
+				exit()
+
+			elif message == "!clear":
+				self.conf.queue = []
+				time.sleep(1)
+				self.mumblebot.updateComment()
+
+			elif message == "!stopall":
+				self.conf.queue = []
+				self.mumblebot.mumble.sound_output.clear_buffer()
+				self.conf.skip = True
+				time.sleep(1)
+				self.mumblebot.updateComment()
+
+			elif message.startswith("!speed"):
+				try:
+					if self.conf.speedchange(message[7:]):
+						self.mumblebot.mumble.users[actor].send_text_message(
+								"Speed changed to " + str(message[7:]) + "x")
+					else:
+						self.mumblebot.mumble.users[actor].send_text_message(
+							"Please provide a speed between 0.5 and 2")
+				except:
+					self.mumblebot.mumble.users[actor].send_text_message(
+							"Please provide a speed between 0.5 and 2")
+
+	def died(self): self.conf.serverDied = True
+
+"""
+Class containing all configurables for the bot
+As per PEP8, constants are declared in CAPITALS
+"""
+class Configurables:
+	def __init__(self, config, messages):
+		self.messages = messages
+
+		self.SERVER = config["host"]
+		self.NICK = config["user"]
+		self.PASSWD = config["password"]
+		self.PORT = config["port"]
+		self.CERTFILE = config["certfile"]
+		self.KEYFILE = config["keyfile"]
+
+		self.playbackSpeed = 1 # Sanitisation required
+		self.playing = False
+		self.skip = False
+
+		self.queue = []
+		self.serverDied = False
+
+	# Updates the queue of songs to play and the corresponding message
+	# in the Messages object
+	def updateQueue(self):
+		titles = []
+		for song in self.queue:
+			command = ["youtube-dl", "-e", song] # Get title only
+			# Runs above command and directs stdout to a pipe
+			with sp.Popen(command, stdout=sp.PIPE) as p: 
+				# Reads title from stdout and decodes into a usable string
+				title = str(p.stdout.readline().decode('UTF-8').strip())
+			titles.append(title)
+
+		# Fill the queue comment with current songs
+		self.messages.queueMessage = "<p>Current queue:</p>"
+		if len(titles) > 0:
+			for title in titles:
+				self.messages.queueMessage += "\n<p>" + title + "</p>"
+		else:
+			self.messages.queueMessage += "\n<p>No songs in queue</p>"
+
+	# Changes the currently playback speed by adding a filter
+	# into FFMPEG
+	def speedchange(self, target):
+		try:
+			target = float(target)
+		except ValueError:
+			print("[!] Invalid Speed Received.")
+			return False
+
+		if target <= 2 and target >= 0.5: # Limits of the ffmpeg filter.
+			self.playbackSpeed = target
+			self.messages.setPlaybackSpeedMessage(target)
 			return True
 		else:
 			return False
-	except:
-		print("[!] Invalid Speed Received.")
-		return False
+				
 
-mumble = pymumble_py3.Mumble(server, nick, password=passwd, port=port, certfile=certfile, keyfile=keyfile)
-mumble.callbacks.set_callback(pymumble_py3.callbacks.PYMUMBLE_CLBK_TEXTMESSAGERECEIVED, message_received)
-mumble.callbacks.set_callback(pymumble_py3.callbacks.PYMUMBLE_CLBK_DISCONNECTED, died)
-mumble.start()
-mumble.is_ready()   #wait for Mumble to get ready to avoid errors after startup
-botUser = mumble.users.myself
-updateComment()
+"""
+Class containing all messages that may be sent/set by the bot
 
-while True:
-	print("start Processing")
-	print("Queue:", queue)
-	if len(queue) > 0:
-		url = queue.pop(0)
-		command = ["youtube-dl", "-e", url]
-		with sp.Popen(command, stdout=sp.PIPE) as p:
-			currentSongMessage = "<p>Currently Playing:</p>"
-			currentSongMessage += "<p>"
-			currentSongMessage += str(p.stdout.readline().decode('UTF-8').strip())
-			currentSongMessage += "</p>"
+This includes blocks of text in the Mumble status field for a user
+"""
+class Messages:
+	def __init__(self):
+		# Help message is stored in helpmessage.html to reduce clutter
+		with open("helpmessage.html") as f:
+			self.helpmessage = f.read()
 
-		command = ["youtube-dl","-f","bestaudio", url, "--buffer-size", "2M", "-o", "-"]
-		wave_file = sp.Popen(command, stdout=sp.PIPE).stdout
-		# Convert and play wave file
-		command = ["ffmpeg", "-i", "-", "-acodec", "pcm_s16le", "-f", "s16le", "-ab", "192k", "-ac", "1", "-ar", "48000", "-fflags", "nobuffer","-filter:a","atempo=" + str(float(playbackspeed)),  "-"]
-		sound = sp.Popen(command, stdout=sp.PIPE, stderr=sp.DEVNULL, stdin=wave_file, bufsize=1024)
-		print("playing")
-		playing = True
-		updateComment()
-		while True:
-			if skip:
-				skip = False
-				break
-			raw_music = sound.stdout.read(1024)
-			if not raw_music:
-				break
-			#mumble.sound_output.add_sound(audioop.mul(raw_music, 2, vol))   #adjusting volume
-			mumble.sound_output.add_sound(raw_music)
+		self.queueMessage = "<p>Current Queue</p>\
+							<p>No songs in queue</p>"
 
-		print("Buffer filled")
-		updateComment()
+		self.currentSongMessage =	"<p>Currently Playing:</p>\
+									<p>Nothing!</p>"
 
-	while mumble.sound_output.get_buffer_size() > 0.5:  #
-		time.sleep(0.01)
-	playing = False
-	currentSongMessage = "<p>Currently Playing:</p>\
-		\n<p>Nothing!</p>"
-	print("sleep")
-	updateComment()
-	time.sleep(2)
-	if serverDied:
-		exit()
+		self.playbackSpeedMessage = "<p>Playback speed: 1x</p>"
+
+	# Sets the playback speed message based on the
+	# Configurables object passed to it
+	def setPlaybackSpeedMessage(self, Configurables): 
+		self.playbackSpeedMessage = "<p>Playback speed: {0}x</p>".format(
+				str(Configurables.playbackSpeed))
+
+bot_instance = MumbleBot()
+bot_instance.startBot()
+bot_instance.mainLoop()
